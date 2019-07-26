08a4d9b5d565518612f6ea96e299f58548901758
AinazZ <ziyazova.aynaz@mail.ru>
2019-07-26 08:48:20 +0300

    Работаю над Router

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-
+import './Game.css';
 import Header from './header';
 import GameBoard from './game-list';
 
 export default class Game extends Component {
   render() {
     return (
-      <div className="TicTacToe">
+      <div className="Game">
         <Header />
         <GameBoard />
       </div>
